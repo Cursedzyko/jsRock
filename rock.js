b47cd74924324cc206c3bf2026cb7b83bd938b1f
Zafarbek <zyunusov@student.42wolfsburg.de>
2023-04-09 21:08:48 +0300

    start button done

--- rock.js
+++ rock.js
@@ -1,73 +1,96 @@
-function getComputerChoice()
-{
-	let b = Math.round(Math.random() * 2);
-	if (b === 0)
-		return ("Rock");
-	else if (b === 1)
-		return ("Paper");
-	else (b === 2)
-		return ("Scissors");
-}
+const startButton = document.querySelector("#startbutton");
+console.log(startButton);
 
-function playRoundUtils(p, c, a)
-{
-	let win = "You win " + a[p].toLowerCase() + " beats " + a[c].toLowerCase();
-	let lose = "You lose " + a[c].toLowerCase() + " beats " + a[p].toLowerCase();
-	if (c === p)
-		return("Draw");
-	else if ((p != 2) && (c != 2))
-	{
-		if (p < c)
-			return(win);
-		else
-			return(lose);
-	}
-	else if ((p === 2) || (c === 2))
-	{
-		if ((c === 1) || (p === 0))
-			return(lose);
-		else if ((p === 1) || (c === 0))
-			return(win);
-	}
-}
+const game = document.querySelector("#game");
+game.style.display = "none";
 
-function playRound(playerSelection, computerSelection)
-{
-	const a = ["SCISSORS", "PAPER", "ROCK"];
-	let p = 0;
-	let c = 0;
-	for(let i = 0; i < 3; i++)
-	{
-		if (playerSelection.toUpperCase() === a[i])
-			p = i;
-		if (computerSelection.toUpperCase() === a[i])
-			c = i;
-	}
-	return (playRoundUtils(p, c, a));
-}
-
-function game()
-{
-	let l = 0;
-	let w = 0;
-	for (let i = 0; i < 5; i++)
-	{
-		let b = prompt("Enter rock, paper or scissors: ");
-		let c = playRound(b, getComputerChoice());
-		console.log(c);
-		if (c.includes("win"))
-			w += 1;
-		else if (c.includes("lose"))
-			l += 1;
-	}
-	console.log("You won " + w + " times and computer " + l + " times");
-
-	if (w > l)
-		console.log("You win");
-	else if (w < l)
-		console.log("Computer win");
+startButton.onclick = function () {
+	if (game.style.display !== "none")
+		game.style.display = "none"
 	else
-		console.log("Draw");
-}
+	{
+		startButton.style.display = "none";
+		game.style.display = "block";
+	}
+};
 
-game();
+
+
+
+
+
+
+// function getComputerChoice()
+// {
+// 	let b = Math.round(Math.random() * 2);
+// 	if (b === 0)
+// 		return ("Rock");
+// 	else if (b === 1)
+// 		return ("Paper");
+// 	else (b === 2)
+// 		return ("Scissors");
+// }
+
+// function playRoundUtils(p, c, a)
+// {
+// 	let win = "You win " + a[p].toLowerCase() + " beats " + a[c].toLowerCase();
+// 	let lose = "You lose " + a[c].toLowerCase() + " beats " + a[p].toLowerCase();
+// 	if (c === p)
+// 		return("Draw");
+// 	else if ((p != 2) && (c != 2))
+// 	{
+// 		if (p < c)
+// 			return(win);
+// 		else
+// 			return(lose);
+// 	}
+// 	else if ((p === 2) || (c === 2))
+// 	{
+// 		if ((c === 1) || (p === 0))
+// 			return(lose);
+// 		else if ((p === 1) || (c === 0))
+// 			return(win);
+// 	}
+// }
+
+// function playRound(playerSelection, computerSelection)
+// {
+// 	const a = ["SCISSORS", "PAPER", "ROCK"];
+// 	let p = 0;
+// 	let c = 0;
+// 	for(let i = 0; i < 3; i++)
+// 	{
+// 		if (playerSelection.toUpperCase() === a[i])
+// 			p = i;
+// 		if (computerSelection.toUpperCase() === a[i])
+// 			c = i;
+// 	}
+// 	return (playRoundUtils(p, c, a));
+// }
+
+// function game()
+// {
+// 	const prompt=require("prompt-sync")({sigint:true}); 
+// 	let l = 0;
+// 	let w = 0;
+// 	for (let i = 0; i < 5; i++)
+// 	{
+// 		let b = prompt("Enter rock, paper or scissors: ");
+// 		let c = playRound(b, getComputerChoice());
+// 		console.log(c);
+// 		if (c.includes("win"))
+// 			w += 1;
+// 		else if (c.includes("lose"))
+// 			l += 1;
+// 	}
+// 	console.log("You won " + w + " times and computer " + l + " times");
+
+// 	if (w > l)
+// 		console.log("You win");
+// 	else if (w < l)
+// 		console.log("Computer win");
+// 	else
+// 		console.log("Draw");
+// }
+
+// game();
